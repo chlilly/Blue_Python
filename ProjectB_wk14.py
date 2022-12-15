784de507f906dc59b3d0f91fde811c3cac92f3e7
chlilly <clilly70@gmail.com>
2022-12-14 20:04:22 -0500

    initial commit

---
+++ ProjectB_wk14.py
@@ -0,0 +1,40 @@
+import boto3
+
+dynamodb = boto3.resource(
+    'dynamodb', 
+    region_name='us-east-1',
+    aws_access_key_id='AKIA3TPPBVOB26NG6PGC',
+    aws_secret_access_key='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')
+
+table = dynamodb.create_table(
+    TableName='online_users',
+    KeySchema=[
+        {
+            'AttributeName': 'gamertag',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'system',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'gamertag',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'system',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+
+
+print("Table status:", table.table_status)

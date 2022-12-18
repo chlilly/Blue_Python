7aca1a0434f82f44fbf0549a42111f98787cdf13
chlilly <clilly70@gmail.com>
2022-12-18 13:56:14 -0500

    added items

--- additemsWK14.py
+++ additemsWK14.py
@@ -4,18 +4,21 @@ dynamodb = boto3.client('dynamodb')
 # Insert data within the Item_3 variable. 
 response = dynamodb.put_item(
     
-    Item_1={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Smoother'}
-	Item_2={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Summer Daze'}
-	Item_3={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Chillin'}
-	Item_6={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Afternoon Daze'}
-	Item_7={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Hazy Morning'}
-	Item_8={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'The Cool Out'}
-	Item_9={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Step Away'}
-	Item_10={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Bliss'}
+Item_1={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Smoother'}
+Item_2={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Summer Daze'}
+Item_3={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Nature Calls'}
+Item_4={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Chillville'}
+Item_5={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Recline'}
+Item_6={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Chillin'}
+Item_7={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Afternoon Daze'}
+Item_8={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Hazy Morning'}
+Item_9={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'The Cool Out'}
+Item_10={'Artist': 'S': 'The Cashmere Collective','SongTitle': 'S': 'Step Away'}
+
     
     ReturnConsumedCapacity='TOTAL',
     TableName='Grooves',
     
-    ), 
+     ), 
 
 
